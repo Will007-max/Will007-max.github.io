29e2fca067dfdce89797b1b08e93ad5a26028a3d
William <djampatapi@yahoo.fr>
2021-03-31 19:01:54 +0200

    re-new site

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Organizing my first ML project",
+        "excerpt":"Here are some words about how I wrote and organized my first project. Who know, it may be useful in some context. I. Context Completing my data science bootcamp, I wanted to realize a great interesting project allowing me to develop new skills and deeply apply the majort part of...","categories": ["machinelearning"],
+        "tags": [],
+        "url": "/machinelearning/first_project/",
+        "teaser": "/assets/images/william_phot.jpg"
+      }]
